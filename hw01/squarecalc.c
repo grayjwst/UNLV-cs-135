438441330ff10ba08bfff23847338fa8d76bafe5
grayjwst <grysnadams09@gmail.com>
2025-02-24 14:58:48 -0700

    fixed styling

--- hw01/squarecalc.c
+++ hw01/squarecalc.c
@@ -2,14 +2,14 @@
 
 int main() {
     printf("Hello! \nI'm a program that will calculate the perimeter and area of a square for you.\nJust give me the length of a side and I'll figure out the rest.\n");
-   
-int length;
-printf("Please provide the measurement of one side of the square: ");
-scanf ("%d" , &length);
-printf("The perimeter of your square is %d\n" , length*4);
-printf("The area of your square is %d\n" , length*length);
+    
+    int length;
+    printf("Please provide the measurement of one side of the square: ");
+    scanf ("%d" , &length);
+    printf("The perimeter of your square is %d\n" , length*4);
+    printf("The area of your square is %d\n" , length*length);
 
-return 0;
+    return 0;
 
 }
 
