593b7a2d4469ea9e4c190b5b7ad21c31f0eb1de6
grayjwst <grysnadams09@gmail.com>
2025-02-27 17:26:57 -0700

    powerbill step1

---
+++ hw02/powerbill.c
@@ -0,0 +1,27 @@
+
+
+// how many KWHRS used (scanf function)
+// multiply int KWHRS by 0.61 (price)
+// multiply KWHRS by 0.095 (county)
+// multiply KWHRS by 0.0325 (city)
+// add all numbers together, add 4.50
+
+
+#include <stdio.h>
+
+int main() {
+
+  printf("how many KWHRS\n");
+
+
+
+
+
+}
+
+
+
+//int main() {
+  //printf("Hello, everybody! It is I, lovable, furry old Grover!\n");
+  //return 0;
+//}
