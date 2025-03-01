25627619205ad806a7fb106645f380b1ee8cb75d
grayjwst <grysnadams09@gmail.com>
2025-02-28 18:12:00 -0700

    2nd working version

--- hw02/powerbill.c
+++ hw02/powerbill.c
@@ -1,6 +1,6 @@
 
 
-// how many KWHRS used (scanf function)
+// how many KWHRS used
 // multiply int KWHRS by 0.61 (price)
 // multiply KWHRS by 0.095 (county)
 // multiply KWHRS by 0.0325 (city)
@@ -9,19 +9,33 @@
 
 #include <stdio.h>
 
+const float price = 0.61;
+const float countySurcharge = 0.095;
+const float cityTax = 0.0325;
+const float stateCharge = 4.50;
+
 int main() {
 
-  int KWHRS = 1;
-  printf("the bill for %d kilowatts is:\n" , KWHRS);
+  int KWHRS = 100.2; // doesn't do decimals?
+  printf("The bill for %d kilowatts is:\n" , KWHRS);
 
-  printf("\tThe base cost is %f\n" , KWHRS*0.61);
-  printf("\tCounty surcharge is %f\n" , KWHRS*0.095);
-  printf("\tcity tax is %f\n" , KWHRS*0.0325);
-  printf("\tstate fees 4.5\n");
-  printf("\ttotal is %f\n" , KWHRS*0.61+KWHRS*0.095+KWHRS*0.0325+4.5 );
+  // TODO: update equations to use constants and create new vars for each charge
+
+  float base = KWHRS*price;
+  printf("\tThe base cost is: %f\n" , base);
+
+  float county = KWHRS*countySurcharge;
+  printf("\tThe county surcharge is: %f\n" , county);
+
+  float city = KWHRS*cityTax;
+  printf("\tThe city tax is: %f\n" , city);
+
+  float state = stateCharge;
+  printf("\tThe state fees are: %f\n" , state);
+
+  float total = base+county+city+state;
+  printf("\tThe total is: %f\n" , total);
   
-
-
 
 }
 
