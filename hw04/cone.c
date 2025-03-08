77fc719d0a60bf31b466e6a7af10738a55ca8b7d
grayjwst <grysnadams09@gmail.com>
2025-03-07 21:34:19 -0700

    first version

--- hw04/cone.c
+++ hw04/cone.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <math.h>
+<<<<<<< HEAD
 #include <string.h> // so we can use strlen
+=======
+>>>>>>> 576a950 (fisrt version)
 
 int main(){
     
     float h; // height
     float d; // diameter
+<<<<<<< HEAD
     char units[100];
 
     printf("What is the height of the cone? ");
@@ -33,5 +37,21 @@ int main(){
     
     float V = (A*h)/3; // volume
     printf("And a volume of %.4f cubic %s\n", V, units);
+=======
+    
+
+    printf("whats the height and diameter ");
+    scanf("%f %f", &h, &d);
+    //printf("the diameter is %f", d);
+
+    float pie = 3.1459;
+    float A = pie*(d/2)*(d/2); // area
+    float C = 2*pie*(d/2); // circumference 
+    double S = sqrt(pow(h,2)+pow(d/2,2)); // slant height
+    
+    float SA = A + (1/2)*C*S; 
+    printf("the surface area is %f\n", SA);
+    
+>>>>>>> 576a950 (fisrt version)
   
 }
